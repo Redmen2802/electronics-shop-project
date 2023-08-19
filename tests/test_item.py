6a1dec79564aa652a20147b265262a2521c0b525
Aleksandr Shibanov <shadow.sanya28@gmail.com>
2023-08-20 00:35:41 +0500

    реализовал класс и написал тесты

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,15 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+def test_item():
+    item = Item("Планшет", 10000, 3)
+    assert item.name == "Планшет"
+    assert item.price == 10000
+    assert item.quantity == 3
+def test_calculate_total_price():
+    item = Item("Планшет", 10000, 3)
+    assert item.calculate_total_price() == 30000
+def test_apply_discount():
+    item = Item("Планшет", 10000, 2)
+    Item.pay_rate = 0.5
+    item.apply_discount()
+    assert item.price == 5000
